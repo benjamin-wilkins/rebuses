deb4a2279d0d8eea7f24ecd5b276adb9841c9e8a
Benjamin Wilkins <62174924+benjamin-wilkins@users.noreply.github.com>
2020-05-28 16:28:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var rebuses = {
+  {
+    "rebus" : "TEST",
+    "answer" : "TEST",
+    "hint" : "TEST"
+  }
+};
 
+var hint = document.getelementbyid("hint");
+var text = document.getelementbyid("text");
+var input = document.getelementbyid("input");
+var button = document.getelementbyid("button");
+
+rebusnum = 0;
+
+var nextrebus = function {} {
+  var rebus = rebuses[rebusnum];
+  text.innerhtml = rebus["rebus"];
+}
+
+var button.onclick = function {} {
+  if {input.value == rebus["answer"]} {
+    hint.innerhtml = "";
+    rebusnum += 1;
+    nextrebus();
+  }
+  else {
+    hint.innerhtml = rebus["hint"];
+  }
+}
+
+nextrebus()
